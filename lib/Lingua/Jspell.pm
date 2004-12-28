f1aef10addb41c2a800c7e986f65388452978f28
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2004-12-28 22:17:06 +0000

    Empty module
    git-svn-id: https://natura.di.uminho.pt/svn/main/Lingua/Jspell@1693 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

---
+++ lib/Lingua/Jspell.pm
@@ -0,0 +1,72 @@
+package Lingua::Jspell;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Lingua::Jspell - The great new Lingua::Jspell!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Lingua::Jspell;
+
+    my $foo = Lingua::Jspell->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Alberto Simões, C<< <ambs@di.uminho.pt> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-lingua-jspell@rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org>.  I will be notified, and then you'll automatically
+be notified of progress on your bug as I make changes.
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2004 Alberto Simões, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Lingua::Jspell
