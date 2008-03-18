fa8649d1b0f5fa29a16dc5080816a17ae06dfcb4
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2008-03-18 17:02:38 +0000

    ainda nao compila...
    git-svn-id: https://natura.di.uminho.pt/svn/main/Lingua/Jspell@5490 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 
 static char * Version_ID[] = {
     " International Jspell Version " VERSION,
-    " Copyright (c), 1994-2005 by Ulisses Pinto & José João Almeida",
+    " Copyright (c), 1994-2008 by Ulisses Pinto & José João Almeida",
     " ",
     " Based on ispell, Copyright (c), 1983, by Pace Willisson",
     " International version Copyright (c) 1987, 1988, 1990, 1991, 1992, 1993,",
