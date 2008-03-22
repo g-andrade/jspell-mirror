4d5eb1fad97304996c365a3dabe1742e5932e1b6
ambs <ambs@f5ed0195-1f1e-0410-9bbf-87de9e03ec61>
2008-03-22 16:22:05 +0000

    Added a function from libc. It will not compile so easily... but let try
    git-svn-id: https://natura.di.uminho.pt/svn/main/Lingua/Jspell@5561 f5ed0195-1f1e-0410-9bbf-87de9e03ec61

--- src/mkstemp.c
+++ src/mkstemp.c
@@ -1,4 +1,81 @@
 
+static const char letters[] =
+"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+int
+__gen_tempname (char *tmpl, int flags, int kind)
+{
+	int len;
+	char *XXXXXX;
+	static uint64_t value;
+	uint64_t random_time_bits;
+	unsigned int count;
+	int fd = -1;
+	int save_errno = errno;
+	struct_stat64 st;
+
+#define ATTEMPTS_MIN (62 * 62 * 62)
+
+#if ATTEMPTS_MIN < TMP_MAX
+	unsigned int attempts = TMP_MAX;
+#else
+	unsigned int attempts = ATTEMPTS_MIN;
+#endif
+
+	len = strlen (tmpl);
+	if (len < 6 || strcmp (&tmpl[len - 6], "XXXXXX")) {
+		__set_errno (EINVAL);
+		return -1;
+    }
+
+	XXXXXX = &tmpl[len - 6];
+
+#ifdef RANDOM_BITS
+	RANDOM_BITS (random_time_bits);
+#else
+# if HAVE_GETTIMEOFDAY || _LIBC
+	{
+	    struct timeval tv;
+	    __gettimeofday (&tv, NULL);
+	    random_time_bits = ((uint64_t) tv.tv_usec << 16) ^ tv.tv_sec;
+	}
+# else
+	random_time_bits = time (NULL);
+# endif
+#endif
+
+	value += random_time_bits ^ __getpid ();
+
+	for (count = 0; count < attempts; value += 7777, ++count) {
+		uint64_t v = value;
+
+		/* Fill in the random bits.  */
+		XXXXXX[0] = letters[v % 62];
+		v /= 62;
+		XXXXXX[1] = letters[v % 62];
+		v /= 62;
+		XXXXXX[2] = letters[v % 62];
+		v /= 62;
+		XXXXXX[3] = letters[v % 62];
+		v /= 62;
+		XXXXXX[4] = letters[v % 62];
+		v /= 62;
+		XXXXXX[5] = letters[v % 62];
+		fd = __open (tmpl, (flags & ~ACCESSPERMS) | O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
+
+		if (fd >= 0) {
+			__set_errno (save_errno);
+			return fd;
+		} else if (errno != EEXIST) {
+			return -1;
+		}
+	}
+
+	__set_errno (EEXIST);
+	return -1;
+}
+
+
 int mkstemp (char *template) {
 	return __gen_tempname (template, 0, 0);
 }
